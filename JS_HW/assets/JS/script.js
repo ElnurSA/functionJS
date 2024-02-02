107def85baa18df0d9798d80ca37e6d787d814e0
ElnurSA <elnurms@code.edu.az>
2024-02-02 21:41:27 +0400

    hw added

---
+++ JS_HW/assets/JS/script.js
@@ -0,0 +1,87 @@
+//1st question
+
+// let n =43;
+
+// if(n%3==0 && n%7==0){
+//     console.log("Bolunur")
+// }else{
+//     console.log("Bolunmur")
+// }
+
+//2nd question
+
+// let a = 7
+// let fact = 1;
+// if(a>0){
+//     for(i=1; i <= a; i++){
+//         fact *= i;
+//     }
+//     console.log(fact);
+// }else{
+//     console.log("number is negative")
+// }
+
+//3rd
+
+// var nums = [1,2,3,4,5,6];
+// var res = 0;
+// let fakt = 1;
+// for(i = 0; i < nums.length; i++){
+//     fakt =1
+//     if(nums[i]%2==0){
+        
+//         for(f = 1; f <= nums[i]; f++){
+//             fakt *= f;
+//         }
+//         res += fakt
+//     }
+// }
+// console.log(res)
+
+//4th
+
+// let mail = "cavid@code.edu.az";
+// let password = 12345;
+
+// function login(mail, password){
+//     if(mail == "cavid@code.edu.az" && password == 12345){
+//         console.log("Girish olundu");
+//     }else{
+//         console.log("Mail ve yaxud password sehvdir");
+//     }
+// }
+
+// login(mail, password)
+
+//5th
+
+// var nums = [1,2,3,4,5,6,7,8];
+// let sum = 0;
+
+// function sumOdd(nums){
+//     for(i = 0; i < nums.length; i++){
+//         if(nums[i] % 2 != 0){
+//             sum+=nums[i];
+//         }
+//     }
+//     console.log(sum)
+// }
+
+// sumEven(nums)
+
+//6th
+
+// var nums = [12,34,55,73, 12, 23, 43, 92, 31];
+// let count = 0;
+
+// function countEven(nums){
+//     for(i = 0; i < nums.length; i++){
+//         if(nums[i] % 2 == 0){
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+// countEven(nums)
+
---
+++ PracticeJS/assets/js/script.js
@@ -0,0 +1,175 @@
+"use strict"
+
+// let surname = "test";
+// alert(surname);
+
+// let person = prompt("Please enter you name:");
+
+// if(person == null){
+//     console.log("invalid");
+// }else{
+//     console.log(person);
+// }
+
+// let elem = confirm("Press a button!");
+
+// console.log(elem);
+
+let stu1 = {
+    id:1,
+    name: "Tunzale",
+    surname: "Memmedova",
+    age:24
+}
+
+let stu2 = {
+    id:2,
+    name: "Oruc",
+    surname: "Mehrabov",
+    age:26
+}
+
+let stu3 = {
+    id:3,
+    name: "Ilaham",
+    surname: "Abasli",
+    age:0
+}
+
+let stu4 = {
+    id:4,
+    name: "Omer",
+    surname: "Elesgerli",
+    age:16
+}
+
+// let result = stu4.name + " " + stu4.surname;
+
+// console.log(result);
+
+const students = [stu1, stu2, stu3, stu4];
+
+// for(let i = 0; i < students.length; i++){
+//     let result = students[i].name + " " + stu4.surname;
+
+//     console.log(result)
+// }
+
+// let sumAge = 0;
+
+// for(let i = 0; i < students.length; i++){
+//     sumAge += students[i].age
+
+// }
+
+// let avarageAges = sumAge / students.length;
+
+// if(avarageAges < 50){
+//     console.log("Young")
+// }else{
+//     console.log("Old");
+// }
+
+// console.log(avarageAges);
+
+// document.querySelector("h1").innerText = studnets[1].name;
+
+// const elmes = [1,3,5,7,8,9];
+
+// for (const items of elems) {
+//     console.log(item)
+// }
+
+// for (const imem of students) {
+//     console.log(item.name)
+// }
+
+// const product = {
+//     name: "Samsung",
+//     price: 500
+// }
+
+// for (const key in product) {
+//    console.log(key + " " + product[key])
+// }
+
+// for (const stu of students) {
+//     for (const key in object) {
+//         if(key == "name"){
+//             let objectData = `${key} - ${stu[key]}`
+//             console.log(objectData)
+//         }
+//     }
+// }
+
+// function showText(){
+
+//     let isActive = true;
+//     if(isActive){
+//         return
+//     }
+//     return ("tested");
+// }
+
+// showText();
+
+
+// function showName(name,surname,address, age){
+//     const fullData = `${name}${surname}${address}${age + 40}`
+//     console.log(fullData);
+// }
+
+// showName("Tofig", "Nasibli", "Hezi", 19);
+
+// function sum (num = 100){
+//     console.log(num + 200);
+// }
+
+// sum(1);
+
+// function getStudentsByAge(students){
+//     let countAge = 0;
+//     for(const stu of students){
+
+//         if(stu.age > 20){
+//             countAge++;
+//         }
+        
+//     }
+
+//     countAge **=2;
+//     return countAge
+
+// }
+
+// console.log(getStudentsByAge(students))
+
+
+// let a = 5;
+
+// a =100;
+
+// let b = a;
+// console.log(b);
+
+// let elems1 = [1,2,3,4,5,6];
+// let elems2 = elems1;
+
+// elems2[2] = 200;
+
+// console.log(elems2[2])
+
+let status = true;
+
+if(status){
+    let elem = 100;
+    console.log("yes");
+}
+
+function test(){
+    let surname = "test";
+}
+
+test();
+
+// console.log(elem);
